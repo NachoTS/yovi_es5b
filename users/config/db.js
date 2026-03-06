bc49407934905b34879f649e4bd4474f6810c0b9
NachoTS <UO245469@uniovi.es>
2026-03-06 17:24:12 +0100

    database: añadir imagen y variables de despliegue #80
    Añadir imagen Docker y variables de despliegue en github y
    docker-compose para la base de datos.

--- users/config/db.js
+++ users/config/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME ?? "yovi-es5b-dev",
+  process.env.DB_USER ?? "root",
+  process.env.DB_PASSWORD ?? "ADMSIS123$",
   {
     host: process.env.DB_HOST, // Esto ahora valdrá 'mysql-db' gracias a Docker
     dialect: 'mysql',
