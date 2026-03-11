872918036d96c921ff0ba3782166d90bd523068a
tonipdd <tonipdd@protonmail.com>
2026-03-11 21:56:16 +0100

    Fix conexión con base de datos

--- users/config/db.js
+++ users/config/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER ?? "root",
   process.env.DB_PASSWORD ?? "ADMSIS123$",
   {
-    host: "172.17.0.1", // dirección interna de host de docker
+    host: process.env.DB_HOST ?? "database",
     dialect: 'mysql',
     logging: false
   }
