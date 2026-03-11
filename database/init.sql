df7d71359c7cefb40e0e75f9b9092841b2222f40
NachoTS <UO245469@uniovi.es>
2026-03-11 22:15:18 +0100

    Revert "Fix conexión con base de datos"
    This reverts commit 872918036d96c921ff0ba3782166d90bd523068a.

--- database/init.sql
+++
@@ -1,7 +0,0 @@
--- Resetear permisos de root para todas las direcciones
-DELETE FROM mysql.user WHERE User='root' AND Host != 'localhost';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'ADMSIS123$';
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'ADMSIS123$';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
--- users/config/db.js
+++ users/config/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER ?? "root",
   process.env.DB_PASSWORD ?? "ADMSIS123$",
   {
-    host: process.env.DB_HOST ?? "database",
+    host: "172.17.0.1", // dirección interna de host de docker
     dialect: 'mysql',
     logging: false
   }
